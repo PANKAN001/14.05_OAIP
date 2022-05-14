3908651a0857e816e82a0f94ce582cf88e24acff
PANIKAN001 <95694958+PANKAN001@users.noreply.github.com>
2022-05-14 12:58:20 +0300

    Add

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public class Phone
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"Смартфон {this.Name}; цена: {this.Price}";
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonMess_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Изучаю WPF");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string text = textBox1.Text;
+            if (text != "")
+            {
+                MessageBox.Show(text);
+            }
+            else
+            {
+                MessageBox.Show("Введите что-то в текстовое поле");
+            }
+        }
+    }
+}
